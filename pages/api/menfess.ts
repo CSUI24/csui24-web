8dc99dcde7601f0cb1c131bf9c498029a21a24b3
Muhammad Fauzan <fauzannmuhh@gmail.com>
2025-06-10 07:54:31 +0700

    chore: add filtering

--- pages/api/menfess.ts
+++ pages/api/menfess.ts
@@ -56,6 +56,10 @@ export default async function handler(
       });
     }
     const filter = [
+      "ade azurat",
+      "kontol",
+      "memek",
+      "pantek",
       "slot gacor",
       "gacor maxwin",
       "maxwin",
