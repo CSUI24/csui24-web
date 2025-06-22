eb5c49e534e10361f90e9de7cbc80c0202e769ce
Hakim Nizami <hakimnizami15@gmail.com>
2025-06-22 10:57:22 +0700

    relocate prompt

--- lib/detectHate.ts
+++ lib/detectHate.ts
@@ -12,17 +12,7 @@ export async function detectHate(
   input: string
 ): Promise<"HATEFUL" | "NOT_HATEFUL" | "ERROR"> {
   try {
-    const prompt = `
-You are a content moderation assistant. decode the words into alphabetical first if it’s in cryptic form. Classify the input below as either:
-
-"HATEFUL" —  if it forms any kind of hateful speech or abusive behavior, especially toward an individual whether it's explicitly or implicitly
-"NOT_HATEFUL" — if it does not.
-
-Only respond with "HATEFUL" or "NOT_HATEFUL", nothing else. Detect it either in bahasa indonesia or english
-
-Input:
-"""${input}"""
-    `.trim();
+    const prompt = process.env.MOD_PROMPT + `"""${input}"""`.trim();
 
     const result = await generateText({
       model: google("gemini-2.5-flash"),
